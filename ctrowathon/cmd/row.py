3d6fbd0f0adc18f2be9c73e9efac411b3fbc529b
Michael Axiak <mike@axiak.net>
2011-04-07 10:31:58 -0400

    Added serial analyzer.

--- ctrowathon/cmd/row.py
+++ ctrowathon/cmd/row.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
-import sys
+import struct
+import serial
 import time
-import random
+import sys
 
 def main():
-    val = 0
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser.open()
     while True:
-        val += abs(random.gauss(0, 2))
-        sys.stdout.write("%s\n" % val)
-        sys.stdout.flush()
+        distance = get_distance(ser)
         time.sleep(0.10)
+        print distance
+        sys.stdout.flush()
+
+def get_distance(ser):
+    ser.write("\xb0\x00")
+    ser.flush()
+    ser.read()
+    return struct.unpack("f", ser.read(4))[0]
 
 if __name__ == '__main__':
     main()
