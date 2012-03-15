bbe70c3bccbe10b866d1bae991c0e2c04d5292ce
Michael Axiak <mike@axiak.net>
2012-03-15 18:38:22 -0400

    Initial gh-pages

---
+++ example/media/js/main.js
@@ -0,0 +1,129 @@
+function max(x, y) {
+  return (x > y) ? x : y;
+}
+
+function min(x, y) {
+  return (x < y) ? x : y;
+}
+
+
+(function ($, window) {
+  var unitMaps = [
+    ["meters", 1, "meter"],
+    ["miles", 0.0006213711, "mile"],
+    ["football fields", 0.00364537766, "football field"],
+    ["boneless skinless chicken breasts", 6.92125, "boneless skinless chicken breast"]
+  ];
+
+  var timeoutScale = 1;
+
+
+  var utils = {
+    "addCommas": function (number) {
+      number += '';
+      x = number.split('.');
+      x1 = x[0];
+      x2 = x.length > 1 ? '.' + x[1] : '';
+      var rgx = /(\d+)(\d{3})/;
+      while (rgx.test(x1)) {
+	x1 = x1.replace(rgx, '$1' + ',' + '$2');
+      }
+      return x1 + x2;
+    },
+
+    "createUnitDom": function (distance) {
+      window.currentDistance = distance;
+      var parentDom = $("<div class='distance-wrapper'/>").css({opacity: 0});
+      for (var i = 0; i < unitMaps.length; ++i) {
+        var intval = Math.round(distance * unitMaps[i][1]);
+        var currentDistance = this.addCommas(intval);
+        var child = $("<span>" + currentDistance + " " + unitMaps[i][intval == 1 ? 2 : 0] + "</span>")
+                     .addClass("distance-info").addClass(unitMaps[i][0].replace(" ", "-"));
+        parentDom.append(child);
+      }
+      var display = $("#distance-display");
+      display.children().fadeTo(200 * timeoutScale, 0, undefined, function () {
+        $(this).remove();
+      });
+      display.append(parentDom);
+      parentDom.fadeTo(200 * timeoutScale, 1);
+    },
+
+    "initializeMap": function ($map) {
+      window.mapInitialized = true;
+      var self = this;
+      var latlng = new google.maps.LatLng(42.37110, -71.04376);
+      var m = -3.1007751937984497e-05;
+      var b = 10.651162790697676;
+      var myOptions = {
+        zoom: Math.round(min(max(m * window.currentDistance + b, 6), 14)),
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        disableDefaultUI: true
+      };
+      var map = new google.maps.Map($map[0],
+                                    myOptions);
+      this.drawCircle(map, latlng);
+      setInterval(function () {
+        self.drawCircle(map, latlng);
+      }, 500 * timeoutScale);
+    },
+
+    "drawCircle": function (map, center) {
+      var oldCircle = this.circle;
+      if (this.circle) {
+        this.circle.setRadius(window.currentDistance);
+      } else {
+        this.circle = new google.maps.Circle({
+          strokeColor: "#FF0000",
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: "#FF0000",
+          fillOpacity: 0.35,
+          map: map,
+          clickable: false,
+          center: center,
+          radius: window.currentDistance
+        });
+      }
+    },
+
+    "updateClock": function (time) {
+      var minutes = Math.floor(time / 60);
+      var seconds = Math.round(time % 60);
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      var clock = $("#clock");
+      clock.text("" + minutes + ":" + seconds);
+      var roundUp = Math.ceil(time / 1800) * 1800;
+      var difference = roundUp - time;
+      if (difference > 450) {
+        clock.css({color: "#000"});
+      } else {
+        clock.css({color: "rgb(0," + Math.round((450 - difference) / 450 * 255) + " , 0)"});
+      }
+    }
+  };
+
+  $(function () {
+    window.mapInitialized = false;
+    $(".map-display").css({width: $(document).width() - $(".twitter-feed").width() - 60});
+
+    var distanceWaiting = false;
+
+    var distance = 1500;
+    var time = 433;
+
+    setInterval(function () {
+      time += 200 * timeoutScale / 1000;
+      distance += (-Math.log(Math.random()) / .1);
+            utils.createUnitDom(distance);
+            utils.updateClock(time);
+            if (!window.mapInitialized) {
+              utils.initializeMap($("#map-display"));
+            }
+}, 200 * timeoutScale);
+
+  });
+})(jQuery, window);
